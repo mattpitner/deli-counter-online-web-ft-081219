30b4ad3af342f357f0e3cbe7e60c2dc0142089f8
Matt Pitner <mattpitner09@gmail.com>
2019-08-19 15:38:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,9 +19,11 @@ def take_a_number(katz_deli,name)
 
 end 
 
-def now_serving
+def now_serving(katz_deli)
   if katz_deli.empty?
     puts "There is nobody waiting to be served!"
+  else 
+    puts "Currently serving #{katz_deli}"
     
   
 end
